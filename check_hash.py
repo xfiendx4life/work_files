b94c697bc1bfd24d6520aeb45d53ab4ec3a3fc15
xfiendx4life <xfiendx4life@gmail.com>
2017-05-05 12:53:01 +0300

    Added percents as a parameter

--- check_hash.py
+++ check_hash.py
@@ -24,35 +24,41 @@ def get_text(filename):
 	return data
 
 def normalize(text):
+	#text = text.replace('\n\n', '\n')
 	return text.lower()
 
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
+try:
+	p = sys.argv[1]
+	var = sys.argv[2]
+	perc = int(sys.argv[3])
 
-p = sys.argv[1]
-var = sys.argv[2]
+	for path, subdirs, files in os.walk(p):
+		for filename in files:
+			f = os.path.join(path, filename)
+			fileList.append(f)
 
-for path, subdirs, files in os.walk(p):
-	for filename in files:
-		f = os.path.join(path, filename)
-		fileList.append(f)
+	if var == '-h':
+		for item in fileList:
+			hashList.append(get_hash_md5(item))
+		for i in range (len(hashList)-1):
+			for j in range(i+1,len(hashList)):
+				if hashList[i]== hashList[j]:
+					print(fileList[i] + '\n' + fileList[j])
+					print("_______________________________")
+	elif var == '-t':
+		for item in fileList:
+			textList.append(normalize(get_text(item)))
+		for i in range(len(textList)):
+			for j in range(i + 1, len(textList)):
+				sim = similar(textList[i],textList[j])
+				#print(sim)
+				if sim >= perc/100:
 
-if var == '-h':
-	for item in fileList:
-		hashList.append(get_hash_md5(item))
-	for i in range (len(hashList)-1):
-		for j in range(i+1,len(hashList)):
-			if hashList[i]== hashList[j]:
-				print(fileList[i] + '\n' + fileList[j])
-				print("_______________________________")
-elif var == '-d':
-	for item in fileList:
-		textList.append(normalize(get_text(item)))
-	for i in range(len(textList)):
-		for j in range(i + 1, len(textList)):
-			sim = similar(textList[i],textList[j])
-			#print(sim)
-			if sim >= 0.5:
-
-				print(fileList[i] + '\n' + fileList[j] + '\n' + '--------%s' % sim)
-				print("_______________________________")
+					print(fileList[i] + '\n' + fileList[j] + '\n' + '--------%s' % sim)
+					print("_______________________________")
+	else:
+		print("-h for hash checking, -t for text checking")
+except:
+	print('Error missed one parameter \n "-h" for hash checking, "-t" for text checking')
